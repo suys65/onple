7b48ea0f6ecc6fcd9aa8a36be93aae1fcb5c0add
suys65 <suysinu65@gmail.com>
2023-11-09 22:26:02 +0900

    버튼구현

--- algorithm.py
+++ algorithm.py
@@ -21,18 +21,18 @@ def get_recommendations(item):
     rules['antecedents_items'] = rules['antecedents'].apply(lambda x: next(iter(x)) if len(x) > 0 else None)
 
     # 추출한 값으로 비교
-    recommended_category = rules[rules['antecedents_items'] >= item]['consequents']
+    recommended_category = rules[rules['antecedents_items'] == item]['consequents']
 
     # Load the product data
     with open('C:\onple\onple\data\product.pkl', 'rb') as f:
         products = pd.read_pickle(f)
     recommended_categoryi = recommended_category.apply(lambda x: next(iter(x)) if len(x) > 0 else None)
-
+    #print(recommended_categoryi)
     # Filter the products that belong to the recommended category
-    category_products = products[products['중 카테고리'] <= recommended_categoryi.iloc[0]]
+    category_products = products[products['중 카테고리'] == recommended_categoryi.iloc[0]]
     
     # Find the product with the highest order quantity in the category
     recommended_product = category_products.sort_values('주문량', ascending=False).iloc[0]
 
     return recommended_product
-#print(get_recommendations('인테리어식물'))
+print(get_recommendations('인테리어식물'))
--- app.py
+++ app.py
@@ -1,46 +1,3 @@
-# # app.py
-# from flask import Flask, render_template
-# from flask import Flask, request
-# from algorithm import get_recommendations
-# from flask import Flask, request, render_template
-# import pickle
-
-# app = Flask(__name__)
-
-
-# # Load product data
-# import pandas as pd
-
-# with open('C:\onple\onple\data\product.pkl', 'rb') as f:
-#     products = pd.read_pickle(f)
-
-
-# @app.route('/product/<product_id>', methods=['GET'])
-# def product_page(product_id):
-#     # Find the category of the clicked product
-#     product = products[products['product_id'] == product_id]  #상품코드
-#     category = product['category'].values[0]      #중 카테고리
-    
-#     # Get recommendations
-#     recommendations = get_recommendations(category)
-    
-#     # Render the product page with recommendations
-#     return render_template('product.html', product=product, recommendations=recommendations)
-
-
-# @app.route('/')
-# def home():
-#     return render_template('index.html')
-# if __name__ == '__main__':
-#     app.run(debug=True)
-
-
-# # 박정한 추가
-
-# @app.route('/product', methods=['POST'])
-# def product_form():
-#     product_id = request.form.get('number')
-#     return redirect(url_for('product_page', product_id=product_id))
 
 # app.py
 from flask import Flask, render_template, request, redirect, url_for
@@ -64,14 +21,15 @@ def product_page(product_id):
     # Find the category of the clicked product
     product = products[products['상품코드'] == product_id]  #상품코드
     category = product['중 카테고리'].values[0]      #중 카테고리
-    print(product)
+    # print(product)
     # Get recommendations
     recommendations = get_recommendations(category)
-    return render_template('product.html', product=product, recommendations=recommendations.iloc[0]) # 추천 상품 데이터프레임의 첫 번째 행을 전달합니다.
+    print(recommendations)
+    return render_template('product.html', product=product, recommendations=recommendations) # 추천 상품 데이터프레임의 첫 번째 행을 전달합니다.
 
 @app.route('/product', methods=['POST'])
 def product_form():
-    product_id = request.form.get('number')
+    product_id = request.form.get('product_code')  # 'product_code'를 통해 상품 코드를 받습니다.
 
     # Check if the product_id exists in the data
     if product_id not in products['상품코드'].values:
@@ -79,9 +37,19 @@ def product_form():
 
     return redirect(url_for('product_page', product_id=product_id))
 
-@app.route('/')
-def home():
-    return render_template('index.html')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+@app.route('/', methods=['GET', 'POST'])
+def home():
+    # 상품 DataFrame을 불러옵니다.
+    products = pd.read_pickle('C:\onple\onple\data\product.pkl')
+    products.rename(columns={'상품코드': 'product_code', '상품명': 'product_name'}, inplace=True)
+
+    # DataFrame에서 랜덤으로 상품을 선택합니다.
+    selected_products = products.sample(10)
+
+    # selected_products를 HTML 파일에 전달하여 렌더링합니다.
+    # 각 행을 사전 형태로 변환합니다.
+    return render_template('index.html', products=selected_products.to_dict('records'))
+
+if __name__ == "__main__":
+    app.run()
